1b01187cda68417012c8829fafd9c88029f61388
nickzmoro <nicolasmororuiz@gmail.com>
2025-05-28 21:01:30 -0300

    chore: add React Router and React Icons, restructure main component for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { Router } from "./routes/routes";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </StrictMode>
 );
